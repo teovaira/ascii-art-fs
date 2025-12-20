88a4f95e9195fe0a8484b816a975488385c52b6a
vxanthio <basiliki.martios1@gmail.com>
2025-12-20 16:53:02 +0200

    feat(renderer): implement renderer logic for empty input and single character

--- renderer/renderer.go
+++ renderer/renderer.go
@@ -1,5 +1,15 @@
 package renderer
 
-func rendererASCII(s string) string {
-	return s
+func rendererASCII(input string, banner map[rune][]string) string {
+	result := ""
+	for _, ch := range input {
+		rows := banner[ch]
+		for i, row := range rows {
+			result += row
+			if i != len(rows)-1 {
+				result += "\n"
+			}
+		}
+	}
+	return result
 }
--- renderer/renderer_test.go
+++ renderer/renderer_test.go
@@ -6,8 +6,36 @@ import (
 
 func TestEmptyInput(t *testing.T) {
 	input := ""
-	output := rendererASCII(input)
+	banner := map[rune][]string{}
+	output := rendererASCII(input, banner)
 	if input != output {
 		t.Errorf("Error")
+	}
+}
+func TestSingleCharacter(t *testing.T) {
+	input := "A"
+	expected := `A1
+A2
+A3
+A4
+A5
+A6
+A7
+A8`
+	banner := map[rune][]string{
+		'A': {
+			"A1",
+			"A2",
+			"A3",
+			"A4",
+			"A5",
+			"A6",
+			"A7",
+			"A8",
+		},
+	}
+	output := rendererASCII(input, banner)
+	if expected != output {
+		t.Error("Error")
 	}
 }
