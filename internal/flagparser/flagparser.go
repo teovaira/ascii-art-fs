428b1da522b3dad9f9838712b31b055ae8c3a641
vxanthio <basiliki.martios1@gmail.com>
2026-01-23 19:05:13 +0200

    feat(flagparser): add argument count validation and update tests

--- internal/flagparser/flagparser.go
+++ internal/flagparser/flagparser.go
@@ -1,7 +1,12 @@
 package flagparser
+
 import "errors"
-func ParseArgs (args []string) error {
-	if len(args)<2 {
+
+func ParseArgs(args []string) error {
+	if len(args) < 2 {
+		return errors.New("error")
+	}
+	if len(args) > 5 {
 		return errors.New("error")
 	}
 	return nil
--- internal/flagparser/flagparser_test.go
+++ internal/flagparser/flagparser_test.go
@@ -1,13 +1,27 @@
 package flagparser_test
-import (
-	 "testing"
-"ascii-art-color/internal/flagparser"
-)
-func TestParseArgs_NoArguments (t *testing.T) {
-args:=[]string{"./ascii-art"}
-err:=flagparser.ParseArgs(args)
-if err==nil {
-	t.Errorf("Error was expected")
-}
 
+import (
+	"ascii-art-color/internal/flagparser"
+	"testing"
+)
+
+func TestParseArgs_NoArguments(t *testing.T) {
+	args := []string{"program"}
+	err := flagparser.ParseArgs(args)
+	if err == nil {
+		t.Errorf("Error was expected")
+	}
+
+}
+func TestParseArgs_TooManyArgs(t *testing.T) {
+	args := []string{"program",
+		"banner",
+		"--color=red",
+		"substring",
+		"some text",
+		"EXTRA"}
+	err := flagparser.ParseArgs(args)
+	if err == nil {
+		t.Errorf("Error too many args")
+	}
 }
